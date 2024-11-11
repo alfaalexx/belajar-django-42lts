0dce35535351bd1277d8c1589dc39a649d430cf9
alfaalexx <alfaalex02@gmail.com>
2024-11-11 10:26:31 +0700

    belajar manajemen templates django

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, 'blog.html')
+    return render(request, 'blog/blog.html')
 
 def cari(request):
     return HttpResponse('<h1>ini adalah halaman cari blog</h1>')
