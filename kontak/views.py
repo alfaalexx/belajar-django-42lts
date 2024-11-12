81a78e014c60dea39408466408e95776e4ea0d64
alfaalexx <alfaalex02@gmail.com>
2024-11-12 09:51:16 +0700

    memperbaiki halaman kontak

--- kontak/views.py
+++ kontak/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'kontak/kontak.html')
+    context ={
+        'title':'Halaman Kontak',
+        'developer':'Alfa Alexandra'
+    }
+    return render(request, 'kontak/kontak.html', context)
